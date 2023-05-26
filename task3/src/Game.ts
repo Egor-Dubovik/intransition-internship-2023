3e082c27594107f22b66f19e45965274612c6e00
Egor Dubovik <egorka54896@gmail.com>
2023-05-26 19:58:17 +0300

    feet: add displayGameTable, displayGameResult, handleUserMove to Game

--- task3/src/Game.ts
+++ task3/src/Game.ts
@@ -1,76 +1,70 @@
-import readline from "readline";
 import CryptoUtils from "./CryptoUtils";
 import GameTable from "./GameTable";
 import Rules from "./Rules";
+import GameInterface from "./GameInterface";
 
 class Game {
   private moves: string[];
   private rules: Rules;
   private key: string;
   private computerMove: number;
+  private gameInterface: GameInterface;
 
   constructor(moves: string[]) {
     this.moves = moves;
     this.rules = new Rules(moves);
-    this.key = CryptoUtils.generateRandomKey(256);
+    this.key = CryptoUtils.generateRandomKey();
     this.computerMove = Math.floor(Math.random() * moves.length) + 1;
+    this.gameInterface = new GameInterface(moves);
+  }
+
+  displayGameTable(): void {
+    const table = new GameTable(this.moves);
+    table.generateTable();
+  }
+
+  displayGameResult(playerMove: number, playerMoveName: string): void {
+    if (playerMove === this.computerMove) {
+      console.log("Draw!");
+      return;
+    } else if (this.rules.isWin(playerMove, this.computerMove)) {
+      console.log("You win!");
+    } else {
+      console.log("You lose!");
+    }
+  }
+
+  handleUserMove(input: string): void {
+    if (!this.rules.isValidMove(input)) {
+      console.log('Invalid move. Please enter a valid move or "?" for help.');
+      return;
+    }
+    if (input === "?") {
+      this.displayGameTable();
+      return;
+    }
+    if (input === "0") {
+      this.gameInterface.closeInterface();
+      return;
+    }
+
+    const playerMove = parseInt(input, 10);
+    const playerMoveName = this.rules.getMoveName(playerMove);
+    console.log(`Your move: ${playerMoveName}`);
+    console.log(`Computer move: ${this.rules.getMoveName(this.computerMove)}`);
+
+    this.displayGameResult(playerMove, playerMoveName);
+    console.log(`HMAC key: ${this.key}`);
   }
 
   run(): void {
-    console.log(
-      `HMAC: ${CryptoUtils.computeHMAC(this.rules.getMoveName(this.computerMove), this.key)}`
-    );
+    const moveName = this.rules.getMoveName(this.computerMove);
+    console.log(`HMAC: ${CryptoUtils.calculateHMAC(moveName, this.key)}`);
     console.log("Available moves:");
-    this.displayMenu();
-
-    const readline = require("readline");
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
+    this.gameInterface.displayMenu();
+    this.gameInterface.getUserMove((input: string) => {
+      this.handleUserMove(input);
     });
-
-    rl.question("Enter your move: ", (input: string) => {
-      if (!this.rules.isValidMove(input)) {
-        console.log('Invalid move. Please enter a valid move or "?" for help.');
-        rl.close();
-        return;
-      }
-
-      if (input === "?") {
-        const table = new GameTable(this.moves);
-        table.generateTable();
-        rl.close();
-        return;
-      }
-
-      const playerMove = parseInt(input, 10);
-      const playerMoveName = this.rules.getMoveName(playerMove);
-
-      console.log(`Your move: ${playerMoveName}`);
-      console.log(`Computer move: ${this.rules.getMoveName(this.computerMove)}`);
-
-      if (playerMove === 0) {
-        console.log("Game exited.");
-      } else if (playerMove === this.computerMove) {
-        console.log("Draw!");
-      } else if (this.rules.isWin(playerMove, this.computerMove)) {
-        console.log("You win!");
-      } else {
-        console.log("You lose!");
-      }
-
-      console.log(`HMAC key: ${this.key}`);
-      rl.close();
-    });
-  }
-
-  private displayMenu(): void {
-    const { moves } = this;
-    for (let i = 0; i < moves.length; i++) {
-      console.log(`${i + 1} - ${moves[i]}`);
-    }
-    console.log("0 - exit");
-    console.log("? - help");
   }
 }
 
