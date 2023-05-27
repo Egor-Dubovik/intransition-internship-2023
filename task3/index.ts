98e7d199976d3e38b94838ca27c109d6e6e0cb19
Egor Dubovik <egorka54896@gmail.com>
2023-05-27 15:05:16 +0300

    refactor: split static main method of claas Main and move args validation to seperate method

--- task3/index.ts
+++ task3/index.ts
@@ -3,13 +3,19 @@ import Game from "./src/Game";
 class Main {
   public static main(): void {
     const moves = process.argv.slice(2);
-    if (moves.length < 3 || moves.length % 2 === 0 || new Set(moves).size !== moves.length) {
-      console.log("Invalid arguments. Please provide an odd number of unique moves.");
-      console.log("Example: npm run dev rock paper scissors lizard Spock");
-      return;
-    }
+    if (!Main.isValidArgs(moves)) return;
     const game = new Game(moves);
     game.run();
   }
+
+  private static isValidArgs(args: string[]): boolean {
+    if (args.length < 3 || args.length % 2 === 0 || new Set(args).size !== args.length) {
+      console.log("Invalid arguments. Please provide an odd number of unique moves.");
+      console.log("Example: npm run dev rock paper scissors lizard Spock");
+      return false;
+    }
+    return true;
+  }
 }
+
 Main.main();
