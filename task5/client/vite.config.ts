8c75099ef1e7f2484aa493acc8e95c57531af280
Egor Dubovik <egorka54896@gmail.com>
2023-06-05 22:33:56 +0300

    refactor: delete setupTests

--- task5/client/vite.config.ts
+++ task5/client/vite.config.ts
@@ -6,4 +6,9 @@ import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    assetsDir: '',
+    sourcemap: false,
+  },
 });
